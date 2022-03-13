18119595afe4527cf9da87f9cba556124de3c200
Rand <randsohail98@gmail.com>
2022-03-13 21:46:28 +0200

    Log In frontend Relates #16

---
+++ client/js/logIn.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+console.log(222);
+const submitButton = document.querySelector('#submitButton');
+const userNameInput = document.querySelector('#username');
+const passwordInput = document.querySelector('#password');
+
+submitButton.addEventListener('click', (e) => {
+  console.log(userNameInput.value);
+  e.preventDefault();
+  fetch('/api/v1/logInUser', {
+    method: 'post',
+    body: JSON.stringify({
+      username: userNameInput.value,
+      password: passwordInput.value,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  }).then((result) => result.json())
+    .then((response) => {
+      console.log('response', response);
+    })
+    .catch((error) => {
+      console.log('1111', error);
+    });
+});
--- server/routes/static.js
+++ server/routes/static.js
@@ -3,6 +3,6 @@ const staticRoutes = require('express').Router();
 const { signUp, logIn } = require('../controllers');
 
 staticRoutes.get('/client/html/signUp.html', signUp);
-staticRoutes.get('/client/html/logIn.html', logIn);
+staticRoutes.get('client/html/logIn.html', logIn);
 
 module.exports = staticRoutes;
