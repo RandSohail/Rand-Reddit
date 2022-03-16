19fbcc61931907796cf1d5a91e4e7decbd612142
Rand <randsohail98@gmail.com>
2022-03-16 19:49:32 +0200

    get all posts and render then in the home page Relates #25

--- client/js/index.js
+++ client/js/index.js
@@ -1,12 +1,8 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable prefer-template */
-/* eslint-disable no-console */
-
 const logInButton = document.querySelector('.logInButton');
 const signUpButton = document.querySelector('.signUpButton');
 const createPost = document.querySelector('.createPost');
 const dropdown = document.querySelector('.dropdown');
-const userName = document.querySelector('.user-name');
+const userName = document.querySelector('#username');
 
 if (document.cookie) {
   logInButton.className = 'hide-element';
@@ -15,17 +11,6 @@ if (document.cookie) {
   dropdown.classList.remove('hide-element');
 }
 
-// function to decoded cookies and get the user id
-function parseJwt(token) {
-  const base64Url = token.split('.')[1];
-  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  const jsonPayload = decodeURIComponent(atob(base64).split('').map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)).join(''));
-  return JSON.parse(jsonPayload);
-}
-
-const { id } = parseJwt(document.cookie);
-
-// render user name on the button
 fetch('/api/v1/userInformation', {
   method: 'get',
   headers: { 'Content-Type': 'application/json' },
@@ -37,14 +22,13 @@ fetch('/api/v1/userInformation', {
     console.log(error);
   });
 
-// get all posts
-fetch('/api/v1/getAllPosts', {
-  method: 'get',
-  headers: { 'Content-Type': 'application/json' },
-}).then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// // function to decoded cookies and get the user id
+// function parseJwt(token) {
+//   const base64Url = token.split('.')[1];
+//   const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+// eslint-disable-next-line max-len
+//   const jsonPayload = decodeURIComponent(atob(base64).split('').map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)).join(''));
+//   return JSON.parse(jsonPayload);
+// }
+
+// const { id } = parseJwt(document.cookie);
--- client/js/post.js
+++ client/js/post.js
@@ -3,8 +3,23 @@ const submitButton = document.querySelector('#submitButton');
 const titleInput = document.querySelector('.title');
 const contentInput = document.querySelector('.content');
 const feedback = document.querySelector('.feedback');
+console.log('object');
+// const userName = document.querySelector('#username');
 
-console.log(2222, contentInput.value);
+// if (document.cookie) {
+//   fetch('/api/v1/userInformation', {
+//     method: 'get',
+//     headers: { 'Content-Type': 'application/json' },
+//   }).then((response) => response.json())
+//     .then((data) => {
+//       console.log(11111);
+//       console.log(data);
+//       userName.textContent = data[0].username;
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// }
 
 submitButton.addEventListener('click', (e) => {
   e.preventDefault();
